9352a2a1aef81d64c586aa29769a3c77d9530b77
Jason Gaudinet <jgaudine@richard.laas.fr>
2018-07-10 11:16:58 +0200

    Detects when workspaces are the same.

--- setworkspace.sh
+++ setworkspace.sh
@@ -2,12 +2,22 @@
 
 # Get current workspace
 old_ws=`sed -n '2p' workspace.txt`
-echo "Old workspace - quickstart/workspace.txt:2: $old_ws\n"
+echo "Old workspace: $old_ws"
 
-# Set new workspace
-sed -i "s,$old_ws,${1},g" workspace.txt
-sed -i "s,$old_ws,${1},g" terminator_config
+# The new workspace is given by the user
 echo "New workspace: $1\n"
 
-# Done
-echo "Replaced every occurrence of $old_ws with $1 in files workspace.txt and terminator_config.\n\nDon't forget to copy terminator_config into terminator config directory using the following commands:\n\nroscd iot_demo\ncp quickstart/terminator_config ~/.config/terminator/config\n"
+if [ $1 = $old_ws ]
+then
+        echo "The workspace is already set to: $old_ws\nExiting...\n"
+	exit 1
+else
+	# Set new workspace
+	sed -i "s,$old_ws,${1},g" workspace.txt
+	sed -i "s,$old_ws,${1},g" terminator_config
+
+	# Done
+	echo "Replaced every occurrence of $old_ws with $1 in files workspace.txt and terminator_config.\n\nDon't forget to copy terminator_config into terminator config directory using the following command:\n\ncp terminator_config ~/.config/terminator/config\n"
+fi
+
+
